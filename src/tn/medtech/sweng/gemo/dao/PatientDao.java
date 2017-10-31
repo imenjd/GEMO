ddffe21fc42f943e5018d22f77b26dc6337c1d64
khairi96 <mejrikhaireddine@gmail.com>
2017-10-31 17:27:33 +0100

    Patient insert and update

--- src/tn/medtech/sweng/gemo/dao/PatientDao.java
+++ src/tn/medtech/sweng/gemo/dao/PatientDao.java
@@ -1,4 +1,7 @@
 package tn.medtech.sweng.gemo.dao;
-
+import tn.medtech.sweng.gemo.entities.Patient;
 public interface PatientDao {
+
+    void insert(Patient patient);
+    void update(Patient patient, int id);
 }
