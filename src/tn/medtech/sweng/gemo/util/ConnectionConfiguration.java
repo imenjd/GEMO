d9b7e2a4826aaa62e3cf554ece02e394d5131be6
khairi96 <mejrikhaireddine@gmail.com>
2017-10-31 17:33:13 +0100

    Patient insert and update and connectionConfiguration

--- src/tn/medtech/sweng/gemo/util/ConnectionConfiguration.java
+++ src/tn/medtech/sweng/gemo/util/ConnectionConfiguration.java
@@ -1,4 +1,19 @@
 package tn.medtech.sweng.gemo.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionConfiguration {
+    public static Connection getConnection (){
+        Connection connection=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/gemo","root","");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+
+    }
 }
