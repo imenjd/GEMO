3e1857ae349cbc4960b17449ce8f145a9a779e73
khairi96 <mejrikhaireddine@gmail.com>
2017-11-01 14:47:50 +0100

    created the class visit inside entities

--- src/tn/medtech/sweng/gemo/entities/Visit.java
+++ src/tn/medtech/sweng/gemo/entities/Visit.java
@@ -1,4 +1,70 @@
 package tn.medtech.sweng.gemo.entities;
 
 public class Visit {
+    private int id;
+    private int id_patient;
+    private String comment;
+
+    private int id_user;
+    private int id_service;
+
+
+    private String date;
+
+    public Visit() {
+
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId_patient() {
+        return id_patient;
+    }
+
+    public void setId_patient(int id_patient) {
+        this.id_patient = id_patient;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public int getId_service() {
+        return id_service;
+    }
+
+    public void setId_service(int id_service) {
+        this.id_service = id_service;
+    }
+
+
+
+
 }
