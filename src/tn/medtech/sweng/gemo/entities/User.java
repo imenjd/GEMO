7264616e54f41867887517a6a38e32917aed6246
khalilzbiss <32544055+khalilzbiss@users.noreply.github.com>
2017-11-09 13:43:14 +0100

    Update User.java

--- src/tn/medtech/sweng/gemo/entities/User.java
+++ src/tn/medtech/sweng/gemo/entities/User.java
@@ -1,4 +1,82 @@
 package tn.medtech.sweng.gemo.entities;
 
 public class User {
+  private String firstName;
+    private String password;
+    private String lastName;
+    private String userName;
+    private String email;
+
+
+    public void setId(int id) {
+        this.id = id;}
+
+    public int getId() {
+        return id;
+    }
+
+    private int id;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private String status;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User (){
+
+    }
+
+    public User(String userName,String password) {
+        this.firstName = firstName;
+        this.password = password;
+        this.lastName = lastName;
+        this.email = email;
+        this.userName=userName;
+    }
 }
