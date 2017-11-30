e4405b0fbe9e190c5f2c6a09881a47657d2bac49
khairi96 <mejrikhaireddine@gmail.com>
2017-11-30 14:30:51 +0100

    Update VisitMain.java

--- src/tn/medtech/sweng/gemo/main/VisitMain.java
+++ src/tn/medtech/sweng/gemo/main/VisitMain.java
@@ -1,4 +1,50 @@
 package tn.medtech.sweng.gemo.main;
 
-public class VisitMain {
+package com.company;
+import javafx.application.Application;
+import java.lang.Exception;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Visit visit = new Visit();
+            Parent Root = FXMLLoader.load(getClass().getResource("/com/company/VisiteFX.fxml"));
+            Scene scene = new Scene(Root, 1000, 600);
+
+            primaryStage.setScene(scene);
+
+            primaryStage.show();
+
+            View.add(scene);
+            View.update(scene);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
+
+
+
+
+
+
+
