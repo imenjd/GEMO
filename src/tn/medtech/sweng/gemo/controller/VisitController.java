013bd73d1c345d19f3f2655f6c1345783ea734ed
khairi96 <mejrikhaireddine@gmail.com>
2017-11-30 14:06:07 +0100

    Update VisitController.java

--- src/tn/medtech/sweng/gemo/controller/VisitController.java
+++ src/tn/medtech/sweng/gemo/controller/VisitController.java
@@ -1,4 +1,32 @@
 package tn.medtech.sweng.gemo.controller;
 
 public class VisitController {
+    public VisitController() {
+    }
+
+    public static void add(int idp,String date, String comment, int ids ){
+        Visit visit =new Visit();
+        visit.setIdp(idp);
+        visit.setDate(date);
+        visit.setComment(comment);
+        visit.setIds(ids);
+
+        VisiteDaoImp dao=new VisiteDaoImp();
+        dao.insert(visit);
+    }
+    public static void update(int idp,String date, String comment,int ids,int id){
+        Visit visit =new Visit();
+        visit.setIdp(idp);
+        visit.setDate(date);
+        visit.setComment(comment);
+        visit.setIds(ids);
+
+        VisiteDaoImp dao=new VisiteDaoImp();
+        dao.update(visit,id);
+    }
+    public static boolean exist(int id){
+        VisiteDaoImp dao=new VisiteDaoImp();
+
+        return dao.exist(id);
+    }
 }
