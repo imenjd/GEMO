1351b6a84d7b9845796dab2ffcb2a14d45fcbfd8
khairi96 <mejrikhaireddine@gmail.com>
2017-11-30 14:42:00 +0100

    Update PatientController.java

--- src/tn/medtech/sweng/gemo/controller/PatientController.java
+++ src/tn/medtech/sweng/gemo/controller/PatientController.java
@@ -1,4 +1,29 @@
 package tn.medtech.sweng.gemo.controller;
 
-public class PatientController {
+
+public class Controller {
+    public Controller() {
+    }
+
+    public static void add(String f,String l,String d){
+        Patient patient=new Patient();
+        patient.setFirstName(f);
+        patient.setLastName(l);
+        patient.setBirthDate(d);
+        PatientDao dao=new PatientDao();
+        dao.insert(patient);
+    }
+    public static void update(String f,String l,String d,int id){
+        Patient patient =new Patient();
+        patient.setFirstName(f);
+        patient.setLastName(l);
+        patient.setBirthDate(d);
+        PatientDao dao=new PatientDao();
+        dao.update(patient,id);
+    }
+    public static boolean exist(int id){
+        PatientDao dao=new PatientDao();
+
+        return dao.exist(id);
+    }
 }
