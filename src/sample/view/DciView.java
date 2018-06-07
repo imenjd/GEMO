b41bee8819295fa203d9a3d44ff768290bae160d
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:40:33 +0100

    Update DciView.java

--- src/sample/view/DciView.java
+++ src/sample/view/DciView.java
@@ -1,4 +1,5 @@
 package sample.view;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -7,186 +8,53 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+
 import  sample.entities.Dci;
 import sample.controller.DciController;
-import sample.entities.User;
 
 
 
-	
-	
+
 	public class DciView {
-		
-		
-		public void winInfo(Scene scene){
-			
-			try {
-				Button btnInfo = (Button) scene.lookup("#btnInfo");
-				btnInfo.setOnAction(e -> {
-					
-					
-					FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/InfoDci.fxml"));
-					Parent root1 = null;
-					try {
-						root1 = (Parent) fxmlLoader.load();
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-					Stage stage = new Stage();
-					stage.setTitle("Information");
-					stage.setScene(new Scene(root1));
-					stage.show();
-					
-				});
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		
-		
-		
-		public static void add(Scene scene ) {
-			
-			
-			Button btnAdd= (Button) scene.lookup("#btnAdd");
-			TextField txtIdFirst = (TextField) scene.lookup("#txtIdFirst");
-			TextField txtNameFirst = (TextField) scene.lookup("#txtNameFirst");
-			Label txtStatusId  = (Label) scene.lookup("#txtStatusId");
-			Label txtStatusIdN  = (Label) scene.lookup("#txtStatusIdN");
-			
-			btnAdd.setOnAction(event -> {
-				
-				int d= Integer.valueOf(txtIdFirst.getText());
-				String s= txtNameFirst.getText();
-				DciController controller = new DciController() ;
-				
-				if(controller.checkId(d)){
-					
-					txtStatusId.setText("This ID already exists!");
-				}
-				else {
-					controller.add(d, s);
-					
-					txtStatusId.setText("Dci Added Successfully!");
-					fillTable(scene);
-					
-				}
-			});
-		}
-		
-		
-		public  static void update (Scene scene ) {
-			
-			try{
-				
-				Button btnUpdate =(Button) scene.lookup("#btnUpdate");
-				TextField txtIdFirst = (TextField) scene.lookup("#txtIdFirst");
-				TextField txtNameFirst = (TextField) scene.lookup("#txtNameFirst");
-				Label txtStatusId  = (Label) scene.lookup("#txtStatusId");
-				Label txtStatusIdN  = (Label) scene.lookup("#txtStatusIdN");
-				TableView table  = (TableView) scene.lookup("#table");
-				TextField txtIdNewFirst = (TextField) scene.lookup("#txtIdNewFirst");
-				
-				
-				
-				btnUpdate.setOnAction(event -> {
-					
-					int id= Integer.valueOf(txtIdFirst.getText()) ;
-					String name= txtNameFirst.getText();
-					int x = Integer.valueOf(txtIdNewFirst.getText());
-					DciController controller = new DciController() ;
-					if(controller.checkId(x)){
-						if(controller.checkId(id) && (id!=x)){
-							
-							txtStatusId.setText("The ID already exists!");}
-						else {
-							
-							controller.update(id, name, x);
-							fillTable(scene);
-							
-							txtStatusId.setText("Dci Updated Successfully!");
-						}
-					}
-					else {
-						
-						txtStatusId.setText("This ID does not exist!");
-					}
-				});
-				
-			}catch (Exception e){
-				e.printStackTrace();
-				
-			}
-			
-		}
-		
-		
-		
-		public  static void delete (Scene scene) {
-			
-			try{
-				
-				Button btnDelete =(Button) scene.lookup("#btnDelete");
-				Label txtStatusId  = (Label) scene.lookup("#txtStatusId");
-				Label txtStatusIdN  = (Label) scene.lookup("#txtStatusIdN");
-				TableView table  = (TableView) scene.lookup("#table");
-				TextField txtIdNewFirst = (TextField) scene.lookup("#txtIdNewFirst");
-				
-				//view
-				btnDelete.setOnAction(event -> {
-					int d = Integer.valueOf(txtIdNewFirst.getText());//view
-					
-					DciController controller = new DciController() ;
-					if(controller.checkId(d)) {
-						controller.delete(d);
-						
-						fillTable(scene);
-						
-						
-						txtStatusId.setText("Dci Deleted Successfully!");
-						
-					}
-					else{
-						
-						txtStatusId.setText("This ID does not exist!");
-					}
-					
-				});
-				
-				
-			}catch (Exception e){
-				e.printStackTrace();
-				
-			}
-			
-		}
+		Parent Root;
+		URL url;
 		
 		
 		public static void fillTable (Scene scene) {
 			
-			try{
-				TableView table  = (TableView) scene.lookup("#table");
+			Button load=(Button) scene.lookup("#LoadDataDCI");
+			
+			load.setOnAction(event ->{
+				try{
+					TableView table  = (TableView) scene.lookup("#table");
+					
+					
+					
+					table.setItems(DciController.fillTable());
+					
+					
+					TableColumn<Dci,String> columnid = new TableColumn<Dci,String>("Identifiant");
+					columnid.setCellValueFactory(new PropertyValueFactory("id"));
+					TableColumn<Dci,String> columnname = new TableColumn<Dci,String>("Nom");
+					columnname.setCellValueFactory(new PropertyValueFactory("name"));
+					
+					table.getColumns().setAll(columnid, columnname);
+					table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);//don't touch this!! remove it and table gets messed up! don't know what it does tho!!!
+					
+				} catch (Exception e){
+					e.printStackTrace();
+				}
 				
-				//view
-				DciController controller =new DciController();
 				
-				table.setItems(controller.fillTable());
-				
-				
-				TableColumn<Dci,Integer> columnid = new TableColumn<Dci,Integer>("id");
-				columnid.setCellValueFactory(new PropertyValueFactory("id"));
-				TableColumn<Dci,String> columnname = new TableColumn<Dci,String>("Dci");
-				columnname.setCellValueFactory(new PropertyValueFactory("name"));
-				
-				table.getColumns().setAll(columnid, columnname);
-				table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-				
-			} catch (Exception e){
-				e.printStackTrace();
-			}
+			});
+			
+			
+			
 			
 		}
+			
+		
 		
 		public void Dashboard(Scene scene,String b){
 			Button Dashboarddci=(Button)scene.lookup("#Dashboard");
@@ -195,29 +63,29 @@ import sample.entities.User;
 				
 				Dashboarddci.setOnAction(event -> {
 					
-					FXMLLoader Loader = new FXMLLoader(getClass().getResource("../view/fxml/Dashboard.fxml"));
-					Parent root = null;
+					url  = getClass().getClassLoader().getResource("sample/view/fxml/Dashboard.fxml");
 					try {
-						root =  Loader.load();
-					} catch (IOException e1) {
-						e1.printStackTrace();
+						Root = FXMLLoader.load(url);
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-					//DciView dview = new DciView();
-					//String u=dview.SessionStorage(scene);
-					UserView a =new UserView();
 					
-					Scene sc2 = new Scene(root, 849, 494);
+					
+					
+					Scene sc2 = new Scene(Root, 720, 540);
 					Stage newstage =  (Stage)((Node) event.getSource()).getScene().getWindow();
 					newstage.setScene(sc2);
 					newstage.show();
+					newstage.setTitle("Accueil");
 					TextField username =(TextField) sc2.lookup("#txtUserName");
 					username.setText(b);
+					
 					HomeView view = new HomeView();
 					view.dci(sc2,b);
 					view.med(sc2,b);
 					view.problem(sc2,b);
 					view.service(sc2,b);
-					view.service(sc2,b);
+					view.patient(sc2,b);
 					view.userpending(sc2,b);
 					view.user(sc2,b);
 					view.Home(sc2,b);
