7cc4babaed3eb1076985a981b34d047e62dc73cf
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 19:05:21 +0100

    Update UserPendingDao.java

--- src/sample/dao/UserPendingDao.java
+++ src/sample/dao/UserPendingDao.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public interface UserPendingDao {
 	void insert(UserPending userp);
-	
+	UserPending selectById(int id);
 	void delete(int id);
-	
-	
 	List<UserPending> selectAll();
 	int  checkId(int id);
 }
