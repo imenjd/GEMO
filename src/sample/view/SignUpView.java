944fa57f6bb1db5e54cfdd78eedbcbf59bdad854
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:45:53 +0100

    Update SignUpView.java

--- src/sample/view/SignUpView.java
+++ src/sample/view/SignUpView.java
@@ -5,31 +5,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import sample.controller.UserController;
 import sample.controller.SignUpController;
 import java.io.*;
+import java.net.URL;
 
 public class SignUpView {
+	Parent Root;
+	URL url;
 	public SignUpView() {
 	}
 	
 	public static void BtnSignUp(Scene scene1) {
 		
-		UserController ctrl = new UserController();
+		
 		Button btnS = (Button) scene1.lookup("#btnSignUp");
 		
 		TextField lastName = (TextField) scene1.lookup("#txtLastName");
 		TextField firstName = (TextField) scene1.lookup("#txtFirstName");
-		TextField email = (TextField) scene1.lookup("#txtEmail");
+		TextField email = (TextField) scene1.lookup("#email");
 		PasswordField pass2 = (PasswordField) scene1.lookup("#txtConfPass");
 		TextField userName = (TextField) scene1.lookup("#txtUserName1");
 		TextField occupation = (TextField) scene1.lookup("#txtOccupation");
 		PasswordField password = (PasswordField) scene1.lookup("#txtPassword1");
 		Label lbl = (Label) scene1.lookup("#lblSign") ;
 		CheckBox cc = (CheckBox)scene1.lookup("#check");
-		
+		btnS.setOnAction(actionEvent -> {
 		try {
-			btnS.setOnAction(actionEvent -> {
+			
 				String l = lastName.getText();
 				String f = firstName.getText();
 				String u = userName.getText();
@@ -46,22 +48,23 @@ public class SignUpView {
 					lbl.setText("Passwords do not match!");
 				}   else if( !(SignUpController.checkUsername(u))){
 					
-					lbl.setText("Choose a different UserName, it alreasy exists!!");
+					lbl.setText("Choose a different UserName, it already exists!!");
 				}
 				
 				else {
 					System.out.println(c);
 					SignUpController ctrler = new SignUpController();
-					ctrler.add(f, l, u, p, p2, em, o, c);
+					ctrler.add(f, l, u, p, em, o, c);
 					lbl.setText("Successfully registered! wait for approval.");
 				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 				
 			});
 			
 			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	
@@ -71,18 +74,18 @@ public class SignUpView {
 			
 			BtnToLogin.setOnAction(event -> {
 				
-				
-				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/fxml/SignIn.fxml"));
-				Parent root1 = null;
+				url  = getClass().getClassLoader().getResource("sample/view/fxml/SignIn.fxml");
 				try {
-					root1 =  fxmlLoader.load();
-				} catch (IOException e1) {
-					e1.printStackTrace();
+					Root = FXMLLoader.load(url);
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 				
-				Scene sc2 = new Scene(root1);
+				
+				Scene sc2 = new Scene(Root);
 				Stage newstage =  (Stage)((Node) event.getSource()).getScene().getWindow();
 				newstage.setScene(sc2);
+				newstage.setTitle("Inscription");
 				newstage.show();
 				UserView view = new UserView();
 				view.BtnLogin(sc2);
