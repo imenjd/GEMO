eed901207cfe72c52478d7760c88d13c7564dd39
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:59:39 +0100

    Update PatientDao.java

--- src/sample/dao/PatientDao.java
+++ src/sample/dao/PatientDao.java
@@ -2,4 +2,8 @@ package sample.dao;
 import sample.entities.Patient;
 public interface PatientDao {
 	void insert(Patient patient);
+	List<Patient> select(String a);
+	List<Patient> selectAll();
+	int searchLast()
+	
 }
