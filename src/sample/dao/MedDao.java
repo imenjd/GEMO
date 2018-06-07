ade03028f422b8b61496ce6bacb871d0e56264c5
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:58:34 +0100

    Update MedDao.java

--- src/sample/dao/MedDao.java
+++ src/sample/dao/MedDao.java
@@ -4,11 +4,7 @@ import java.util.List;
 
 public interface MedDao {
 	void insert(Med med);
-	void update(Med med , int id);
-	void delete(int id);
 	int selectByName(String name);
-	Med selectById ( int id);
-	
 	List<Med> selectAll();
-	int  checkId(int id);
+	
 }
