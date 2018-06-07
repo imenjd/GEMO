027c0102d44836ad4ca8bbd867b22df1966356ce
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:54:43 +0100

    Update InterventionDao.java

--- src/sample/dao/InterventionDao.java
+++ src/sample/dao/InterventionDao.java
@@ -1,7 +1,6 @@
 package sample.dao;
 import sample.entities.Intervention;
 public interface InterventionDao {
-	
 	void insert(Intervention intervention);
 	void delete (int id);
 	int selectByName(String a);
