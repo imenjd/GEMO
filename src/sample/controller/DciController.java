5ab6c252c9db330263512b7c44340e550f37a063
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:03:16 +0100

    Update DciController.java

--- src/sample/controller/DciController.java
+++ src/sample/controller/DciController.java
@@ -9,45 +9,7 @@ import java.util.List;
 
 public class DciController {
 	
-	public DciController(){}
 	
-	
-	public  void delete (int d) {
-		
-		DciDaoImpl dcidao = new DciDaoImpl();
-		dcidao.delete(d);
-	}
-	
-	public  void update (int id, String name , int x) {
-		
-		DciDaoImpl dcidao = new DciDaoImpl();
-		Dci dci = new Dci();
-		
-		dci.setId(id);
-		dci.setName(name);
-		
-		dcidao.update(dci, x);
-		
-	}
-	
-	public  void add (int id, String name ) {
-		
-		DciDaoImpl dciDao1 = new DciDaoImpl();
-		Dci dci = new Dci();
-		dci.setId(id);
-		dci.setName(name);
-		
-		dciDao1.insert(dci);
-		
-	}
-	
-	public boolean checkId (int id) {
-		DciDaoImpl dao = new DciDaoImpl();
-		if (dao.checkId(id) >= 0)
-			return true;
-		else
-			return false;
-	}
 	
 	public static ObservableList fillTable () {
 		
