5a7c2a9a4961ce0bc51ac1700475ebc536a80f1d
hajerkh <31798101+hajerkh@users.noreply.github.com>
2018-06-07 18:49:10 +0100

    Update DciDao.java

--- src/sample/dao/DciDao.java
+++ src/sample/dao/DciDao.java
@@ -5,11 +5,7 @@ import java.util.List;
 public interface DciDao {
 	
 	void insert(Dci dci);
-	void update(Dci dci , int id);
-	void delete(int id);
 	int selectByName(String name);
-	Dci selectById ( int id);
-	
 	List<Dci> selectAll();
-	int  checkId(int id);
+	
 }
